21292d6bda796bac72be3baf5c54c88eba82247c
emanuelhardwell <emanuelnoe16@gmail.com>
2023-02-27 00:13:46 -0600

    feat: Se creo el componente para la busqueda de nuevos gifs

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim().length <= 2) {
+      console.log("Solo se aceptan palabras mayores a 2 valores");
+      return;
+    }
+
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="inputValue"
+          placeholder="Buscar gif"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+      </form>
+    </>
+  );
+};
