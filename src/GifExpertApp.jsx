cfc05203f0c5e26eeb595dbd699e65a4177c074b
emanuelhardwell <emanuelnoe16@gmail.com>
2023-02-27 00:12:13 -0600

    feat: Se agrego funcionalidad al componente padre

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "bob esponja",
+    "patricio",
+    "calamardo",
+  ]);
+
+  const addNewCategory = (category) => {
+    setCategories([category, ...categories]);
+  };
+
   return (
     <div>
       <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={(category) => addNewCategory(category)} />
     </div>
   );
 };
